1aa127881bf9621b63f0b0023d47de305def3ad8
Aryan-Motwani <81680234+Aryan-Motwani@users.noreply.github.com>
2024-04-28 22:53:04 +0530

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./form.css";
 import { addDoc, collection, getDocs } from "firebase/firestore";
-import { db } from "./db";
 import jamun from "./jamun.png";
+import { db } from "./db";
 
 function Form() {
   const [name, setName] = useState("");
